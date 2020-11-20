d59c1dd480c8058c4dce2bea869cf57a34fa07f2
mostmand <mostmand.mh@gmail.com>
2020-11-20 22:29:49 +0330

    Some refactoring

--- Cloneable/CloneableGenerator.cs
+++ Cloneable/CloneableGenerator.cs
@@ -34,12 +34,15 @@ namespace Cloneable
         public void Execute(GeneratorExecutionContext context)
         {
             InjectCloneableAttribute(context);
+            GenerateCloneMethods(context);
+        }
 
+        private void GenerateCloneMethods(GeneratorExecutionContext context)
+        {
             if (context.SyntaxReceiver is not SyntaxReceiver receiver)
                 return;
 
             var classSymbols = GetClassSymbols(context, receiver);
-
             foreach (var classSymbol in classSymbols)
             {
                 context.AddSource($"{classSymbol.Name}_cloneable.cs", SourceText.From(CreateCloneableCode(classSymbol), Encoding.UTF8));
@@ -49,42 +52,58 @@ namespace Cloneable
         private string CreateCloneableCode(INamedTypeSymbol classSymbol)
         {
             string namespaceName = classSymbol.ContainingNamespace.ToDisplayString();
+            string fieldAssignmentsCode = GenerateFieldAssignmentsCode(classSymbol);
 
-            var source = new StringBuilder($@"namespace {namespaceName}
+            return $@"namespace {namespaceName}
 {{
-    {classSymbol.DeclaredAccessibility.ToString().ToLowerInvariant()} partial class {classSymbol.Name}
+    {GetAccessModifier(classSymbol)} partial class {classSymbol.Name}
     {{
         public {classSymbol.Name} Clone()
         {{
             return new {classSymbol.Name}
             {{
-");
-            var fieldNames = GetFieldNames(classSymbol);
-            var fieldAssignments = fieldNames.Select(x => $@"                {x} = this.{x}");
-            source.Append(string.Join(@",
-", fieldAssignments));
-
-            source.Append($@"
+                {fieldAssignmentsCode}
             }};
         }}
     }}
-}}");
-            return source.ToString();
+}}";
         }
 
-        private static IEnumerable<string> GetFieldNames(INamedTypeSymbol classSymbol)
+        private static string GenerateFieldAssignmentsCode(INamedTypeSymbol classSymbol)
         {
-            var fieldNames = new List<string>();
-            foreach (var propertySymbol in classSymbol.GetMembers().OfType<IPropertySymbol>().Where(x => x.SetMethod is not null).Where(x => x.CanBeReferencedByName))
-            {
-                fieldNames.Add($"{propertySymbol.Name}");
-            }
+            var fieldNames = GetFieldsAndProperties(classSymbol);
+            var fieldAssignments = fieldNames.Select(x => $@"                {x} = this.{x}");
+            var fieldAssignmentsCode = string.Join($",{Environment.NewLine}", fieldAssignments);
+            return fieldAssignmentsCode;
+        }
 
-            foreach (var fieldSymbol in classSymbol.GetMembers().OfType<IFieldSymbol>().Where(x => x.CanBeReferencedByName))
-            {
-                fieldNames.Add($"{fieldSymbol.Name}");
-            }
-            return fieldNames;
+        private static string GetAccessModifier(INamedTypeSymbol classSymbol)
+        {
+            return classSymbol.DeclaredAccessibility.ToString().ToLowerInvariant();
+        }
+
+        private static IEnumerable<string> GetFieldsAndProperties(INamedTypeSymbol classSymbol)
+        {
+            IEnumerable<string> properties = GetProperties(classSymbol);
+            IEnumerable<string> fields = GetFields(classSymbol);
+            return properties.Concat(fields);
+        }
+
+        private static IEnumerable<string> GetFields(INamedTypeSymbol classSymbol)
+        {
+            return classSymbol.GetMembers().OfType<IFieldSymbol>()
+               .Where(x => x.CanBeReferencedByName)
+               .Select(fieldSymbol => $"{fieldSymbol.Name}")
+               .ToList();
+        }
+
+        private static IEnumerable<string> GetProperties(INamedTypeSymbol classSymbol)
+        {
+            return classSymbol.GetMembers().OfType<IPropertySymbol>()
+                .Where(x => x.SetMethod is not null)
+                .Where(x => x.CanBeReferencedByName)
+                .Select(propertySymbol => $"{propertySymbol.Name}")
+                .ToList();
         }
 
         private static List<INamedTypeSymbol> GetClassSymbols(GeneratorExecutionContext context, SyntaxReceiver receiver)
@@ -92,20 +111,31 @@ namespace Cloneable
             var options = (context.Compilation as CSharpCompilation).SyntaxTrees[0].Options as CSharpParseOptions;
             var compilation = context.Compilation.AddSyntaxTrees(CSharpSyntaxTree.ParseText(SourceText.From(cloneableAttributeText, Encoding.UTF8), options));
 
-            var attributeSymbol = compilation.GetTypeByMetadataName("Cloneable.CloneableAttribute")!;
+            var cloneableAttributeSymbol = compilation.GetTypeByMetadataName("Cloneable.CloneableAttribute")!;
 
             var classSymbols = new List<INamedTypeSymbol>();
             foreach (var clazz in receiver.CandidateClasses)
             {
-                var model = compilation.GetSemanticModel(clazz.SyntaxTree);
-                var classSymbol = model.GetDeclaredSymbol(clazz)!;
-                if (classSymbol.GetAttributes().Any(ad => ad.AttributeClass.Equals(attributeSymbol, SymbolEqualityComparer.Default)))
+                INamedTypeSymbol classSymbol = GetClassSymbol(compilation, clazz);
+                if (HasAttribute(classSymbol, cloneableAttributeSymbol))
                 {
                     classSymbols.Add(classSymbol);
                 }
             }
 
             return classSymbols;
+        }
+
+        private static bool HasAttribute(INamedTypeSymbol classSymbol, INamedTypeSymbol attributeSymbol)
+        {
+            return classSymbol.GetAttributes().Any(ad => ad.AttributeClass.Equals(attributeSymbol, SymbolEqualityComparer.Default));
+        }
+
+        private static INamedTypeSymbol GetClassSymbol(Compilation compilation, Microsoft.CodeAnalysis.CSharp.Syntax.ClassDeclarationSyntax clazz)
+        {
+            var model = compilation.GetSemanticModel(clazz.SyntaxTree);
+            var classSymbol = model.GetDeclaredSymbol(clazz)!;
+            return classSymbol;
         }
 
         private static void InjectCloneableAttribute(GeneratorExecutionContext context)
