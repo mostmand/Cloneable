0c2cee173b7d5b04308a5809223881ff025ec324
mostmand <mostmand.mh@gmail.com>
2020-11-20 22:30:06 +0330

    Remove an unnecessary using statement

--- Cloneable/CloneableGenerator.cs
+++ Cloneable/CloneableGenerator.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using Microsoft.CodeAnalysis;
+﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace Cloneable
 {
